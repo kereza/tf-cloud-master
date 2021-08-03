118e0a862a73bd2a8501cfb0df551b67e6ce4ab3
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-03 23:05:36 +0200

    fourth commit

--- workspaces.tf
+++ workspaces.tf
@@ -1,14 +1,6 @@
-resource "tfe_workspace" "second" {
-  name         = "second-workspace"
-  organization = tfe_organization.second.id
-  description = "description second-workspace"
-  terraform_version = "0.13.5"
-  //vcs_repo
-}
-
 resource "tfe_workspace" "aws-org" {
   name         = "aws-org"
-  organization = tfe_organization.aws_org.id
+  organization = "main-organization"
   description = "Configures AWS Organization and IAM users/roles"
   terraform_version = "0.13.5"
   vcs_repo {
