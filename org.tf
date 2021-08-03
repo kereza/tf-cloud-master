acfdb5acb5eefbd2a42206692dacefaac98137e0
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-03 21:15:13 +0200

    first commit

--- org.tf
+++ org.tf
@@ -1,8 +1,17 @@
 resource "tfe_organization" "keri" {
-  name  = "first-org"
+  name  = "keri-org"
   email = "kereza85@gmail.com"
   session_timeout_minutes = 20160
   session_remember_minutes = 20160
   collaborator_auth_policy = "password"
   owners_team_saml_role_id  = "wtfrole"
+}
+
+resource "tfe_organization" "first" {
+  name  = "first-org"
+  email = "kereza85@gmail.com"
+  session_timeout_minutes = 20160
+  session_remember_minutes = 20160
+  collaborator_auth_policy = "password"
+  owners_team_saml_role_id  = "first"
 }
