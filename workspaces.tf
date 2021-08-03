8e3ef1427ec88575137c80055d286ea31349eaf7
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-03 21:31:39 +0200

    third commit

--- workspaces.tf
+++ workspaces.tf
@@ -2,6 +2,6 @@ resource "tfe_workspace" "second" {
   name         = "second-workspace"
   organization = tfe_organization.second.id
   description = "description second-workspace"
-  terraform_version = 13.5
+  terraform_version = "0.13.5"
   //vcs_repo
 }
