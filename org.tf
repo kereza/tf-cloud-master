1efd32052be2224b1f1958d44631c68119593a85
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-03 21:24:13 +0200

    second commit

--- org.tf
+++ org.tf
@@ -7,11 +7,11 @@ resource "tfe_organization" "keri" {
   owners_team_saml_role_id  = "wtfrole"
 }
 
-resource "tfe_organization" "first" {
-  name  = "first-org"
+resource "tfe_organization" "second" {
+  name  = "second-org"
   email = "kereza85@gmail.com"
   session_timeout_minutes = 20160
   session_remember_minutes = 20160
   collaborator_auth_policy = "password"
-  owners_team_saml_role_id  = "first"
+  owners_team_saml_role_id  = "second"
 }
