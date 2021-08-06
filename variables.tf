ac2d9d879a938ff8b15b479430f551805e968c3f
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-06 19:39:31 +0200

    new var

--- variables.tf
+++ variables.tf
@@ -1,5 +1,19 @@
-variable "MAIN_AWS_KEY" {}
-variable "MAIN_AWS_SECRET" {}
+variable "MAIN_AWS_KEY" {
+    description = "The main AWS key. Placed manually in the aws-org workspace. Used to configure the master account and additional users"
+    type = string
+}
+variable "MAIN_AWS_SECRET" {
+    description = "The main AWS key. Placed manually in the aws-org workspace. Used to configure the master account and additional users"
+    type = string
+}
+variable "OPS_AWS_KEY" {
+    description = "The AWS KEY used by the CloudOPS system roles. Used for building infrastructure. Placed manually in the aws-org, but distributed as env variable across other workspaces"
+    type = string
+}
+variable "OPS_AWS_SECRET" {
+    description = "The AWS KEY used by the CloudOPS system roles. Used for building infrastructure. Placed manually in the aws-org, but distributed as env variable across other workspaces"
+    type = string
+}
 
 variable "oauth_token_id" {
     description = "Token for GitHUb communication"
