152933496e667a8396ac366b08f3d724fb76ee80
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-07 22:04:40 +0200

    new stuff

--- workspaces.tf
+++ workspaces.tf
@@ -46,7 +46,7 @@ resource "tfe_workspace" "core_workspaces" {
   description = each.value.description
   terraform_version = "0.13.5"
   working_directory = each.value.working_directory
-  //remote_state_consumer_ids = "aws-org"
+  remote_state_consumer_ids = tfe_workspace.aws-org.id
   vcs_repo {
       identifier = each.value.repo
       branch = each.value.branch
