fff4165b36b89d713be7ca21c744f27eccf0d8e4
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-08 10:26:57 +0200

    new stuff

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "workspace_id" {
   value = tfe_workspace.aws-org.id
 }
+
+data "tfe_workspace_ids" "all" {
+  names        = ["*"]
+  organization = "main-organization"
+}
+
+output "workspace_id_all" {
+  value = data.tfe_workspace.all
+}
