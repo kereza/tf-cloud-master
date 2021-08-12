66190596365d575c6cd098dee78eeb501212cb79
Ivan Kerezov <ivan.kerezov@tis.biz>
2021-08-12 13:53:13 +0200

    init

--- variables.tf
+++ variables.tf
@@ -36,7 +36,7 @@ variable "core_workspaces" {
       },
       vpc-prod = {
         working_directory = "vpc"
-        branch = "prod"
+        branch = "main"
         repo = "kereza/tf-core"
         description = "description"
         environment = "prod"
@@ -54,7 +54,7 @@ variable "core_workspaces" {
       },
       security-prod = {
         working_directory = "security"
-        branch = "prod"
+        branch = "main"
         repo = "kereza/tf-core"
         description = "description"
         environment = "prod"
